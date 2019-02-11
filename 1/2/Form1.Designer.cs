8720fbfb291aa73f106d3014b96a60fd8c344d0f
Dimon Mobile <mobik.dimka@gmail.com>
2019-02-11 19:17:46 +0300

    Lab 1 part 2 complete

--- 1/2/Form1.Designer.cs
+++ 1/2/Form1.Designer.cs
@@ -28,20 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(286, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Размер коллекции";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(289, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(219, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(276, 80);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Сгенерировать коллекцию";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 101);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(201, 80);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Сортировать по возрастанию";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(501, 101);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(201, 80);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Сортировать по убыванию";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(87, 230);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(91, 41);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Запрос 1";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(289, 230);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(91, 41);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Запрос 2";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(501, 230);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(91, 38);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Запрос 3";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(24, 308);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(309, 251);
+            this.listBox1.TabIndex = 8;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(339, 308);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(356, 251);
+            this.listBox2.TabIndex = 9;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(365, 438);
+            this.ClientSize = new System.Drawing.Size(707, 586);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
     }
 }
 
--- 1/2/Form1.cs
+++ 1/2/Form1.cs
@@ -16,5 +16,73 @@ namespace _2
         {
             InitializeComponent();
         }
+
+        public delegate bool Comparator(object first, object second);
+        private Comparator algo;
+
+        public void Sort()
+        {
+            for (int i = 0; i < listBox1.Items.Count; ++i)
+            {
+                for (int j = i + 1; j < listBox1.Items.Count; ++j)
+                {
+                    if (algo(listBox1.Items[i], listBox1.Items[j]))
+                    {
+                        object temp = listBox1.Items[i];
+                        listBox1.Items[i] = listBox1.Items[j];
+                        listBox1.Items[j] = temp;
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Int32.TryParse(textBox1.Text, out int count))
+            {
+                listBox1.Items.Clear();
+                Random random = new Random();
+                for (int i = 0; i < count; ++i)
+                {
+                    listBox1.Items.Add(random.Next(100));
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            algo = (object first, object second) => (Int32)first > (Int32)second;
+            Sort();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            algo = (object first, object second) => (Int32)first < (Int32)second;
+            Sort();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            IEnumerable<int> result = listBox1.Items.Cast<int>().Where(x => x > 50);
+            listBox2.Items.Clear();
+            foreach(int item in result)
+                listBox2.Items.Add(item);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            IEnumerable<int> result = listBox1.Items.Cast<int>().Where(x => x >= 30 && x <= 60).OrderBy(x=>x);
+            listBox2.Items.Clear();
+            foreach (int item in result)
+                listBox2.Items.Add(item);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            IEnumerable<int> result = listBox1.Items.Cast<int>().Where(x => x >= 60).OrderByDescending(x => x);
+            listBox2.Items.Clear();
+            foreach (int item in result)
+                listBox2.Items.Add(item);
+        }
     }
 }
