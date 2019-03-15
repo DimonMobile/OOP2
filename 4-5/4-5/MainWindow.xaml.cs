322d1b3045ffeb9ff0f972f79de5e8c61e73524f
Dimon Mobile <mobik.dimka@gmail.com>
2019-03-15 18:03:57 +0300

    Lab 4-5 complete

---
+++ 4-5/4-5/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Forms;
+using System.Drawing.Text;
+using System.Drawing;
+using Microsoft.Win32;
+
+namespace _4_5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFile_click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                TextRange range = new TextRange(textBox.Document.ContentStart, textBox.Document.ContentEnd);
+                FileStream stream = new FileStream(openFileDialog.FileName, FileMode.Open);
+                range.Load(stream, System.Windows.DataFormats.Rtf);
+                stream.Close();
+                fileNameTextBlock.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void SaveFile_click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog saveFileDialog = new Microsoft.Win32.SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                TextRange range = new TextRange(textBox.Document.ContentStart, textBox.Document.ContentEnd);
+                FileStream stream = new FileStream(saveFileDialog.FileName, FileMode.Create);
+                range.Save(stream, System.Windows.DataFormats.Rtf);
+                stream.Close();
+                fileNameTextBlock.Text = saveFileDialog.FileName;
+            }
+        }
+
+        private void NewFile_click(object sender, RoutedEventArgs e)
+        {
+            textBox.Document.Blocks.Clear();
+        }
+
+        private void FontEdit_click(object sender, RoutedEventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+            if (fontDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                TextSelection selection = textBox.Selection;
+                if (selection != null && !selection.IsEmpty)
+                {
+                    selection.ApplyPropertyValue(TextElement.FontFamilyProperty, fontDialog.Font.FontFamily.Name);
+                    selection.ApplyPropertyValue(TextElement.FontSizeProperty, fontDialog.Font.SizeInPoints.ToString());
+                }
+            }
+        }
+
+        private void ColorEdit_click(object sender, RoutedEventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                TextSelection selection = textBox.Selection;
+                if (selection != null && !selection.IsEmpty)
+                {
+                    selection.ApplyPropertyValue(TextElement.ForegroundProperty, colorDialog.Color.Name.ToString());
+                }
+            }
+        }
+
+        private void CutEdit_click(object sender, RoutedEventArgs e)
+        {
+            textBox.Cut();
+        }
+
+        private void CopyEdit_click(object sender, RoutedEventArgs e)
+        {
+            textBox.Copy();
+        }
+
+        private void PasteEdit_click(object sender, RoutedEventArgs e)
+        {
+            textBox.Paste();
+        }
+
+        private void TextBox_textChanged(object sender, TextChangedEventArgs e)
+        {
+            TextRange range = new TextRange(textBox.Document.ContentStart, textBox.Document.ContentEnd);
+            CharactersCountTextBlock.Text = range.Text.Length.ToString();
+        }
+
+        private void OpenExit_click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+    }
+}
