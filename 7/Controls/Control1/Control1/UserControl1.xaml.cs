b4e5cf4d9e1756d29cc2162d5d0e2cb2bd4ef916
Dimon Mobile <mobik.dimka@gmail.com>
2019-04-18 20:37:23 +0300

    Lab7 added but added only

---
+++ 7/Controls/Control1/Control1/UserControl1.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace Control1
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public event EventHandler<EventArgs> FileNameChanged;
+
+        public string FileName
+        {
+            get { return theTextBox.Text; }
+            set { theTextBox.Text = value; }
+        }
+
+        public UserControl1()
+        {
+            InitializeComponent();
+            theTextBox.TextChanged += OnTextChanged;
+        }
+
+        private void theButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog d = new OpenFileDialog();
+            if (d.ShowDialog() == true)
+            {
+                this.FileName = d.FileName;
+            }
+        }
+
+        void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            e.Handled = true;
+            if (FileNameChanged != null)
+                FileNameChanged(this, EventArgs.Empty);
+        }
+    }
+}
