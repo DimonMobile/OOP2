d5b1446d21c59d970073d9db954598494627b7ce
Dmitry <mobik.dimka@gmail.com>
2019-04-18 21:49:58 +0300

    Lab8 base prepared

---
+++ 8/8/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void test_onClick(object sender, RoutedEventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True");
+            try
+            {
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                command.CommandText = "SELECT Id FROM Student";
+                SqlDataReader reader = command.ExecuteReader();
+                MessageBox.Show( ((int)reader.GetValue(0)).ToString());
+                reader.Close();
+            }
+            catch(SqlException ex)
+            {
+                MessageBox.Show(ex.StackTrace);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ 8/scripts/dbo.faculty.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Faculty]
+(
+	ShortName VARCHAR(10) PRIMARY KEY,
+	LongName VARCHAR(30) NOT NULL
+)
