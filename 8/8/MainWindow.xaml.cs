98a0fa26be3a8da4d3626138034681c7c31172c3
Dmitry <mobik.dimka@gmail.com>
2019-04-21 15:06:37 +0300

    Lab8 as i think complete

--- 8/8/MainWindow.xaml.cs
+++ 8/8/MainWindow.xaml.cs
@@ -30,7 +30,43 @@ namespace _8
 
         public MainWindow()
         {
-            InitializeComponent();            
+            InitializeComponent();
+            SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True");
+            connection.Open();
+            SqlCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT Distinct TABLE_NAME FROM information_schema.TABLES";
+            SqlDataReader reader = command.ExecuteReader();
+
+            bool facExists = false;
+            bool studExists = false;
+
+            while (reader.Read())
+            {
+                string currentTableName = (string)reader.GetValue(0);
+                if (currentTableName == "Faculty")
+                    facExists = true;
+                else if (currentTableName == "Student")
+                    studExists = true;
+            }
+            reader.Close();
+
+            if (!facExists)
+            {
+                MessageBox.Show("Faculty table crated");
+                command = connection.CreateCommand();
+                command.CommandText = "CREATE TABLE [dbo].[Faculty] ([ShortName] VARCHAR(10) NOT NULL,[LongName]  VARCHAR(50) NOT NULL, PRIMARY KEY CLUSTERED([ShortName] ASC))";
+                command.ExecuteNonQuery();
+            }
+
+            if (!studExists)
+            {
+                MessageBox.Show("Student table crated");
+                command = connection.CreateCommand();
+                command.CommandText = "CREATE TABLE [dbo].[Student] ([Id]      INT IDENTITY(1, 1) NOT NULL,[Faculty] VARCHAR(10)     NOT NULL,[Name]    VARCHAR(50)     NOT NULL,[Picture] VARBINARY(8000) NULL, CONSTRAINT[FK_Faculty] FOREIGN KEY([Faculty]) REFERENCES[dbo].[Faculty]([ShortName]))";
+                command.ExecuteNonQuery();
+            }
+
+            connection.Close();
         }
 
         private void test_onClick(object sender, RoutedEventArgs e)
@@ -60,18 +96,7 @@ namespace _8
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-
-            _8.Database1DataSet database1DataSet = ((_8.Database1DataSet)(this.FindResource("database1DataSet")));
-            // Load data into the table Student. You can modify this code as needed.
-            _8.Database1DataSetTableAdapters.StudentTableAdapter database1DataSetStudentTableAdapter = new _8.Database1DataSetTableAdapters.StudentTableAdapter();
-            database1DataSetStudentTableAdapter.Fill(database1DataSet.Student);
-            System.Windows.Data.CollectionViewSource studentViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("studentViewSource")));
-            studentViewSource.View.MoveCurrentToFirst();
-            // Load data into the table Faculty. You can modify this code as needed.
-            _8.Database1DataSetTableAdapters.FacultyTableAdapter database1DataSetFacultyTableAdapter = new _8.Database1DataSetTableAdapters.FacultyTableAdapter();
-            database1DataSetFacultyTableAdapter.Fill(database1DataSet.Faculty);
-            System.Windows.Data.CollectionViewSource facultyViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("facultyViewSource")));
-            facultyViewSource.View.MoveCurrentToFirst();
+            FillTablesData();
         }
 
         private void addStudent_onClick(object sender, RoutedEventArgs e)
@@ -81,16 +106,14 @@ namespace _8
             {
                 connection.Open();
                 SqlCommand command = connection.CreateCommand();
-                command.CommandText = "INSERT INTO Student (Faculty, Name) VALUES (@Faculty, @Name)";
+                command.CommandText = "INSERT INTO Student (Faculty, Name, Picture) VALUES (@Faculty, @Name, @Picture)";
 
                 command.Parameters.AddWithValue("@Faculty", facultyTextBox.Text);
                 command.Parameters.AddWithValue("@Name", nameTextBox.Text);
-                //command.Parameters.AddWithValue("@picture", pictureData);
+                command.Parameters.AddWithValue("@Picture", pictureData);
 
                 command.ExecuteNonQuery();
-
-                dataGrid1.UpdateLayout();
-                UpdateLayout();
+                FillTablesData();
             }
             catch (SqlException ex)
             {
@@ -114,6 +137,85 @@ namespace _8
                 FileStream stream = new FileStream(fName, FileMode.Open, FileAccess.Read);
                 BinaryReader br = new BinaryReader(stream);
                 pictureData = br.ReadBytes((int)stream.Length);
+            }
+        }
+        
+        private void FillTablesData()
+        {
+            using (SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True"))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT * FROM Student", connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable dt = new DataTable("Student");
+                adapter.Fill(dt);
+                dataGrid1.ItemsSource = dt.DefaultView;
+            }
+
+            using (SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True"))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT * FROM Faculty", connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable dt = new DataTable("Faculty");
+                adapter.Fill(dt);
+                dataGrid2.ItemsSource = dt.DefaultView;
+            }
+        }
+
+        private void dg_onSelected(object sender, RoutedEventArgs e)
+        {
+            if (dataGrid1.SelectedIndex != -1)
+            {
+                int id = Int32.Parse((dataGrid1.SelectedItems[0] as DataRowView).Row["Id"].ToString());
+                using (SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True"))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand($"SELECT Picture FROM Student WHERE Id={id}", connection);
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        byte[] buf = new byte[8000];
+                        if (!reader.IsDBNull(0) || true)
+                        {
+                            long readed = reader.GetBytes(0, 0, buf, 0, 8000);
+                            using (var ms = new MemoryStream(buf, 0, (int)readed))
+                            {
+                                BitmapImage bi = new BitmapImage();
+                                bi.BeginInit();
+                                bi.CreateOptions = BitmapCreateOptions.None;
+                                bi.CacheOption = BitmapCacheOption.Default;
+                                bi.StreamSource = ms;
+                                bi.EndInit();
+                                bi.Freeze();
+                                BitmapSource source = BitmapSource.Create(2, 2, bi.DpiX, bi.DpiY, PixelFormats.Indexed8, BitmapPalettes.Gray256, buf, 2);
+
+                                imageView.Source = source;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void remove_onClick(object sender, RoutedEventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True");
+            try
+            {
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                command.CommandText = $"DELETE FROM Student WHERE Id={studentIdTextBox.Text}";
+                command.ExecuteNonQuery();
+                FillTablesData();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.StackTrace);
+            }
+            finally
+            {
+                connection.Close();
             }
         }
     }
