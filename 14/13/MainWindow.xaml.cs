2420ba7c5ac80febb3d4920f3f89c14c322f0579
Dimon Mobile <mobik.dimka@gmail.com>
2019-04-30 18:01:32 +0300

    Adapter pattern implemented

---
+++ 14/13/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _13
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void create_click(object sender, RoutedEventArgs e)
+        {
+            CreateDialog dialog = new CreateDialog();
+            dialog.Owner = this;
+            dialog.ShowDialog();
+            var items = dialog.Elements;
+            Random randomGenerator = new Random();
+            foreach(var item in items)
+            {
+                grid.Children.Add(item);
+                Rectangle rectElement = item as Rectangle;
+                RectItemAdapter adapter = new RectItemAdapter(rectElement);
+                adapter.X = randomGenerator.Next(0, (int)(Width - 100));
+                adapter.Y = randomGenerator.Next(0, (int)(Height - 100));
+            }
+        }
+
+        private void createComplex_click(object sender, RoutedEventArgs e)
+        {
+            Builder builder = new Builder();
+            Director director = new Director(builder);
+            director.CreateComplexItem(300, 300);
+            List<UIElement> list = director.GetResult();
+            foreach (var element in list)
+                grid.Children.Add(element);
+        }
+    }
+}
---
+++ 14/13/RectItemAdapter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Shapes;
+
+namespace _13
+{
+    class RectItemAdapter
+    {
+        private Rectangle m_element;
+        public RectItemAdapter(Rectangle element)
+        {
+            m_element = element;
+        }
+        public RectItemAdapter(UIElement element)
+        {
+            m_element = element as Rectangle;
+        }
+        public double X
+        {
+            get
+            {
+                return m_element.Margin.Left;
+            }
+            set
+            {
+                Thickness margin = m_element.Margin;
+                margin.Left = value;
+                m_element.Margin = margin;
+            }
+        }
+        public double Y
+        {
+            get { return m_element.Margin.Top; }
+            set
+            {
+                Thickness margin = m_element.Margin;
+                margin.Top = value;
+                m_element.Margin = margin;
+            }
+        }
+    }
+}
